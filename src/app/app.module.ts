ab4bffdf69ca30f243fdd58cb1ff23fbaa8ea265
Sumit Hansdak <sumit.hansdak@icicilombard.com>
2024-10-25 20:25:21 +0530

    -- Bifurcated into tasks & shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { TasksModule } from './tasks/tasks.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { HeaderComponent } from './header/header.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-    CardComponent
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    SharedModule,
+    TasksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
